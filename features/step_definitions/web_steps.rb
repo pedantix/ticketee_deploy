21107975e1a7c38a650f3255f4c2942a86e8b9bf
Shaun Hubbard <Shaun.Hubbard@reasonandmadness.com>
2013-06-02 16:56:49 -0400

    All the features are complete

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -20,6 +20,28 @@ When(/^I press "([^"]*)"$/) do |button|
   click_button(button) 
 end
 
-Then(/^I should see "([^"]*)"$/) do |text|
-  page.should have_content(text)
+Then(/^I should see "([^"]*)"$/) do |regexp|
+    regexp = Regexp.new(regexp)
+
+  if page.respond_to? :should
+    page.should have_xpath('//*', :text => regexp)
+  else
+    assert page.has_xpath?('//*', :text => regexp)
+  end
 end
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
+
+Then(/^I should see the title "(.*?)"$/) do |title|
+  page.should have_title(title)
+end
+
+
+
